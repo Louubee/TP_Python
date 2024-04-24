393478a60bacae6af4e281529237be36d6000bf0
Louubee <bruderlouis@gmail.com>
2024-04-24 12:39:50 +0200

    Update Cours_1.py

--- Cours_1.py
+++ Cours_1.py
@@ -1,26 +1,57 @@
-#Exercice 1 :
+from random import randrange
+import numpy as np
 
+#Exercice 1 :
 print('My Python Code Forever')
 
-print('My Python \nCode Forever')
+print('My Python \nCode \nForever')
 
-a=1
-b=1.5
-phrase = 'STRING'
-boolean=bool(True) 
-list=[1,2,3]
-tupl=(1,3)
-ens={1,2,3}
-dictionnaire={'name':'Alice', 'age' : 30, 'city': 'wonderland'}
-null=None
+int_type=1
+float_type=1.5
+string_type = 'STRING'
+boolean_type=bool(True) 
+list_type=[1,2,3]
+tupl_type=(1,3)
+ens_type={1,2,3}
+dictionnaire_type={'name':'Alice', 'age' : 30, 'city': 'wonderland'}
+null_type=None
+
 
 my42count='quarante-deux'
 print(len(my42count))
 
+
+
+
+
+
+#10
+my42Type=print(type(boolean_type).__name__)
+
+#11
 compute42=str(21*2)
 print(type(compute42))
 
-ex_12="42424242"
 
+#12
+ex_12="42424242"
 ex_12 = ex_12.replace("42", "quarante-deux")
 print(ex_12)
+
+
+#13
+a = 24
+b = 42
+a = a + b
+b = a - b
+a = a - b
+print(a,b)
+
+
+a = 24
+b = 42
+c=b
+b=a
+a=c
+print(a,b)
+
