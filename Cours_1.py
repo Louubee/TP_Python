a9ef4078eff810794de6ef21241c8440208ae63b
Louubee <bruderlouis@gmail.com>
2024-04-24 10:31:34 +0200

    Initial commit

---
+++ Cours_1.py
@@ -0,0 +1,26 @@
+#Exercice 1 :
+
+print('My Python Code Forever')
+
+print('My Python \nCode Forever')
+
+a=1
+b=1.5
+phrase = 'STRING'
+boolean=bool(True) 
+list=[1,2,3]
+tupl=(1,3)
+ens={1,2,3}
+dictionnaire={'name':'Alice', 'age' : 30, 'city': 'wonderland'}
+null=None
+
+my42count='quarante-deux'
+print(len(my42count))
+
+compute42=str(21*2)
+print(type(compute42))
+
+ex_12="42424242"
+
+ex_12 = ex_12.replace("42", "quarante-deux")
+print(ex_12)
